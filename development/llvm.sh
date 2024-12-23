fabe331eb3de31c3c780b3c607951a7ccd5b7ade
schnow265 <thesnowbox@icloud.com>
2024-12-23 10:16:01 +0100

    removed llvm. compile cause my system to go oom

--- development/llvm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p llvm-build/build
-cd llvm-build/build
-cmake -DCMAKE_INSTALL_PREFIX=$HOME/bin/ -DLLVM_PARALLEL_LINK_JOBS=1 -DLLVM_ENABLE_PROJECTS=clang -DCMAKE_BUILD_TYPE=Release -G "Ninja" ../llvm
